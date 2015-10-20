6a3a08272c92f112fc1c3db4ab6dc7011722b512
Connor Murry <cmurtheepic@gmail.com>
2015-10-20 11:45:55 -0500

    animated mainactivity
    I animated a little wipe left animation for changing views on the apps
    first screen. and a start trans on basics practice activity

--- app/src/main/java/com/spizer/mizer2/Start/MathProblemSelector.java
+++ app/src/main/java/com/spizer/mizer2/Start/MathProblemSelector.java
@@ -1,4 +1,4 @@
-package com.spizer.mizer2.Start;
+package com.spizer.mizer2.start;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -7,8 +7,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.spizer.mizer2.basics.BasicsPractice;
 import com.spizer.mizer2.R;
 import com.spizer.mizer2.notifications.ReminderToPractice;
@@ -61,7 +59,8 @@ public class MathProblemSelector extends AppCompatActivity {
 
     /** called when the user clicks the BASICS button **/
     public void Basics(View view) {
-        Intent BasicView = new Intent(this, BasicsPractice.class);
+        Intent BasicView = new Intent(MathProblemSelector.this, BasicsPractice.class);
+        overridePendingTransition(R.anim.mathproblemtrans1, R.anim.mathproblemtrans2);
         startActivity(BasicView);
     }
 
